c8696c5dc872c356ccfe00d872220120610529d3
mattaLeo <159021786+mattaLeo@users.noreply.github.com>
2025-09-16 19:07:27 +0000

    v1.0b

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
 from flask import Flask, send_file
+from flask_cors import CORS
 import random, json
 
 f = open("Pokemon.json")
 data = json.load(f)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/")
 def index():
